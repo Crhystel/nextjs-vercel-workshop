20bd02071219258a59aebe7ae2fe417a223cd323
Crhystel <crhystelvelasco213@gmail.com>
2025-11-27 20:41:02 -0500

    quitanto nombre de variable mal puesto

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,6 @@
 import { auth } from "@/auth";
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-export default async function middleware(
-  req: NextApiRequestCookies,
-  ev: NextFetchEvent
-) {
+export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const session = await auth();
   if (!session) {
     return NextResponse.redirect(new URL("/api/auth/signin", req.url));
